5a0f43461d16431a3796433cbb726ecb6cc3ac5f
developagil <agilpamungkassc@gmail.com>
2023-04-02 14:19:10 +0700

    register animation

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, unused_import
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:login_register_ui/constants.dart';
+import 'package:login_register_ui/firebase_options.dart';
+import 'package:login_register_ui/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class DefaultFirebaseOptions {
+  static var currentPlatform;
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConstants.APP_NAME,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => const LoginScreen()));
+              },
+              child: const Text("Login"),
+            ),
+            MaterialButton(
+              color: Colors.orange,
+              onPressed: () {},
+              child: const Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
